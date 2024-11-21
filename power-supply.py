98fd3c81edf7b2f72571805fee0ae92894368acf
Kevin F. Ortega <kevin.f.ortega@gmail.com>
2024-11-21 10:34:39 -0800

    updating print messages

--- power-supply.py
+++ power-supply.py
@@ -8,8 +8,8 @@ maxPowerOutlets = 8
 def setPowerOutletState(ip: str, user: str, pword: str, outletNumber: int, state: bool):
 
   # Checkout outlet number is within expected range
-  if outletNumber < 1 or outletNumber > maxPowerOutlets:
-    print("[ERROR]: Specified Outlet Number {} is out of range [{}, {}].".format(outletNumber, 1, maxPowerOutlets))
+  if outletNumber < 0 or outletNumber >= maxPowerOutlets:
+    print("[ERROR]: Specified Outlet Number {} is out of range [{}, {}].".format(outletNumber, 0, maxPowerOutlets-1))
     exit(2)
 
   # Convert boolean to string
@@ -29,33 +29,41 @@ def setPowerOutletState(ip: str, user: str, pword: str, outletNumber: int, state
 
   # Report of request was a success or failure
   if response.status_code == 200 or response.status_code == 204:
-    print("Successfully set outlet {} to {}.".format(outletNumber, stateStr))
-    print(response.text)
+    print("[INFO] Successfully set outlet {} to {}.".format(outletNumber, stateStr))
   else:
-    print("Failed to set outlet {} to {}.".format(outletNumber, stateStr))
+    print("[ERROR] Failed to set outlet {} to {}.".format(outletNumber, stateStr))
     print(response.text)
 
 def showPowerSupplyStatus(ip: str, user: str, pword: str):
-  cmd = 'curl -k -u {}:{} -H "Accept:application/json" --digest https://{}/restapi/relay/outlets/all;/physical_state/'.format(user, pword, ip)
-  cmdToks = cmd.split()
-  print(cmd)
-  print(cmdToks)
-  result = subprocess.run(cmdToks, capture_output=True, text=True)
 
-  if result.returncode != 0:
-    print("[Error]: {}".format(result.stderr))
-    print("Failed to get power supply status. Exiting")
-    exit(1)
+  # Prepare the request to power on/off outlet
+  powerStatusUrl = "http://{}/restapi/relay/outlets/all;/physical_state/".format(ip)
+  headers = {
+    "Accept": "application/json"
+  }
+  auth = requests.auth.HTTPDigestAuth(user, pword)
+
+  # Send request
+  response = requests.get(powerStatusUrl, headers=headers, auth=auth, verify=False)
+
+  if response.status_code == 200 or response.status_code == 207:
+    pass
+  else:
+    print("[ERROR] Failed to query power state.")
+    print(response.text)
+    exit(4)
+
+  toks = response.text[1:-1].split(",")
 
   for outlet in range(maxPowerOutlets):
-    status = "ON" if result.stdout[outlet] == "true" else "OFF"
-    print("Outlet {}: {}".format(outlet+1, status))
+    status = "ON" if toks[outlet] == "true" else "OFF"
+    print("Outlet {}: {}".format(outlet, status))
   
 if __name__ == "__main__":
   ap = argparse.ArgumentParser("Tool to use CI Power Switch")
   ap.add_argument("--status", "-s", action="store_true", help="Show all power outlet's power on status")
-  ap.add_argument("--on", "-t", type=int, help="Power on the specified power outlet. Range is [1,8].") 
-  ap.add_argument("--off", "-f", type=int, help="Power off the specified power outlet. Range is [1,8].")
+  ap.add_argument("--on", "-t", type=int, help="Power on the specified power outlet. Range is [0,7].")
+  ap.add_argument("--off", "-f", type=int, help="Power off the specified power outlet. Range is [0,7].")
   ap.add_argument("--ip", "-i", type=str, default="192.168.0.100", help="Power outlet's IP. Default is %(default)s.")
   ap.add_argument("--user", "-u", type=str, default="admin", help="The user name. Default is %(default)s.")
   ap.add_argument("--pword", "-p", type=str, default="1234", help="The user name's pass. Default is %(default)s.")
@@ -63,7 +71,7 @@ if __name__ == "__main__":
 
   if(args.status):
     showPowerSupplyStatus(args.ip, args.user, args.pword)
-  if(args.on):
+  if(args.on != None):
     setPowerOutletState(args.ip, args.user, args.pword, args.on, True)
-  if(args.off):
+  if(args.off != None):
     setPowerOutletState(args.ip, args.user, args.pword, args.off, False)
